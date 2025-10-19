9ff0aa1c5b826aa8104bc5ba6756cd388e43ad88
Asuka Libessart <sotoka.libessart@gmail.com>
2025-10-19 19:25:26 +0200

    Edit on economy.ts
    + balance, leaderboard, coinflip

--- src/commands/economy.ts
+++ src/commands/economy.ts
@@ -118,7 +118,129 @@ export default {
                 return interaction.reply({
                     embeds: [embed]
                 });
-            }
-        } // end of group "info"
+            };
+
+            if(sub === "balance") {
+                const user = interaction.options.getUser("user") || interaction.user;
+
+                const member = await interaction.guild!.members.fetch(user.id);
+
+                const balance: number = economy.getBalance(user.id);
+
+                const embed = new EmbedBuilder()
+                    .setTitle("Balance")
+                    .setAuthor({
+                        name: member?.displayName ?? user.displayName,
+                        iconURL: member?.displayAvatarURL({size: 128}) ?? user.avatarURL({size: 128})
+                    })
+                    .setDescription(`### JPY${balance.toLocaleString()}`)
+                    .setColor(0xfedfe1)
+                    .setTimestamp()
+                    .setFooter({text: "Use /economy account to see your KannaLuls."});
+
+                return interaction.reply({embeds: [embed]});
+            };
+
+            if(sub === "leaderboard") {
+                const file = path.join(__dirname, "../bot_data/economy.json");
+
+                if(!fs.existsSync(file)) {
+                    return interaction.reply({
+                        content: "❌ No economy data found",
+                        flags: MessageFlags.Ephemeral
+                    });
+                };
+
+                const data = JSON.parse(fs.readFileSync(file, "utf8"));
+
+                const sorted = Object.entries(data)
+                    .map(([id, user]) => {
+                        const u = user as { balance?: number, luls?: number };
+                        return { id, balance: u.balance || 0, luls: u.luls || 0 };
+                    })
+                    .sort((a, b) => b.balance - a.balance)
+                    .slice(0, 10);
+
+                if(sorted.length === 0) {
+                    return interaction.reply({
+                        content: "❌ Nobody has money yet",
+                        flags: MessageFlags.Ephemeral
+                    });
+                };
+
+                const embed = new EmbedBuilder()
+                    .setTitle("🏆 Leaderboard - Top 10 richest users")
+                    .setThumbnail(interaction.guild!.iconURL({size: 512}))
+                    .setDescription(sorted.map((u, i) => `${i + 1}. - <@${u.id}>\nBalance: ${u.balance.toLocaleString()}\nKannaLuls: ${u.luls.toLocaleString()}`).join("\n\n"))
+                    .setColor(0xfedfe1)
+                    .setTimestamp();
+
+                return interaction.reply({
+                    embeds: [embed]
+                });
+
+                
+            };
+        }; // end of group "info"
+
+
+        if(group === "bet") {
+
+            if(sub === "coinflip") {
+                const member = await interaction.guild!.members.fetch(interaction.user.id);
+                const userId = interaction.user.id;
+
+                const choice: string = interaction.options.getString("choice", true);
+
+                const bet: number = interaction.options.getInteger("amount", true);
+
+                if(choice !== "heads" && choice !== "tails") {
+                    return interaction.reply({
+                        content: "❌ Invalid choice! Please choose heads or tails!",
+                        flags: MessageFlags.Ephemeral
+                    });
+                };
+
+                if(bet <= 0) {
+                    return interaction.reply({
+                        content: "❌ Bet must be greater than 0.",
+                        flags: MessageFlags.Ephemeral
+                    });
+                };
+
+                const balance: number = economy.getBalance(userId);
+
+                if(balance < bet) {
+                    return interaction.reply({
+                        content: `❌ You don't have enough money!! You currently have JPY${balance.toLocaleString()}.`,
+                        flags: MessageFlags.Ephemeral
+                    });
+                };
+
+                let side = Math.random() < 0.5 ? "heads" : "tails";
+
+                let resultEmbed = new EmbedBuilder()
+                    .setTitle("🌸 Coin flip result")
+                    .setColor(0xfedfe1)
+                    .setTimestamp()
+                    .setThumbnail(member?.displayAvatarURL({size: 512}) ?? interaction.user.avatarURL({size: 512}));
+
+                if(side === choice) {
+                    const winnings = bet * 2;
+                    await economy.addBalance(userId, bet);
+                    resultEmbed.setDescription(`🎉 It landed on ${side}!!! Congrats!\nYou earned JPY${winnings.toLocaleString()}!`);
+                    return interaction.reply({
+                        embeds: [resultEmbed]
+                    });
+                } else {
+                    await economy.removeBalance(userId, bet);
+                    resultEmbed.setDescription(`😢 It landed on ${side}... I'm sorry...\nYou lost JPY${bet.toLocaleString()}.`);
+                    return interaction.reply({
+                        embeds: [resultEmbed]
+                    });
+                };
+            
+            };
+        }; // end of group bet
     }
 }
